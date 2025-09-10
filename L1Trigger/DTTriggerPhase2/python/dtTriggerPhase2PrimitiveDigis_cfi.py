1f71a9efe807734ccef6eb71d5ffca0071a6992e
Daniel Estrada <daniel.estrada.acevedo@cern.ch>
2025-09-10 14:13:26 +0200

    Include unharcode_sector flag in AM emulator

--- L1Trigger/DTTriggerPhase2/python/dtTriggerPhase2PrimitiveDigis_cfi.py
+++ L1Trigger/DTTriggerPhase2/python/dtTriggerPhase2PrimitiveDigis_cfi.py
@@ -67,6 +67,7 @@ dtTriggerPhase2PrimitiveDigis = cms.EDProducer("DTTrigPhase2Prod",
                                                activateBuffer  = cms.bool(False),
                                                superCelltimewidth = cms.double(400), # in nanoseconds
                                                superCellspacewidth = cms.int32(20), # in number of cells: IT MUST BE AN EVEN NUMBER
+                                               unhardcoded_sectorgt12 = cms.bool(False), # to avoid hardcoding sector 13 and 14 to be 4 and 10
                                                )
 
 dtTriggerPhase2PrimitiveDigis.HoughGrouping      = HoughGrouping
